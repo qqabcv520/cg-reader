8a5ebbea1961a7e06e45b50915cd4bedf0e53d37
米饭 <qqabcv520@outlook.com>
2017-04-13 14:31:28 +0800

    null

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items = [];
 
+  constructor() {
+    for (let i = 0; i < 30; i++) {
+      this.items.push( this.items.length );
+    }
   }
 
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.items.push( this.items.length );
+      }
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
+  }
 }
