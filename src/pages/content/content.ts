6084b63ff0d6b1e6486cd30e683ddf4a4e283547
chenjie <chenjie1995@live.com>
2017-04-17 18:41:46 +0800

    content

---
+++ src/pages/content/content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-content',
+  templateUrl: 'content.html'
+})
+export class ContentPage{
+    constructor() {
+        
+    }
+}
