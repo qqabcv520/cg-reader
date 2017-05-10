4f3a339595fb27df0d67d939d1edf007428d6ee0
chenjie <chenjie1995@live.com>
2017-05-10 17:18:23 +0800

    setting

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from "ionic-angular";
 
 /**
  * The Settings page is a simple form that syncs with a Settings provider
@@ -6,12 +7,27 @@ import { Component } from '@angular/core';
  *
  */
 @Component({
-  selector: 'page-settings',
-  templateUrl: 'settings.html'
+    selector: 'page-settings',
+    templateUrl: 'settings.html'
 })
 export class SettingsPage {
-  // Our local settings object
-  constructor() {
+    // Our local settings object
+    constructor(private toastCtrl: ToastController) {
+
+    }
+
+    upDate() {
+        this.showToast("无更新");
+    }
+
     
-  }
+
+    private showToast(str: string) {
+        let toast = this.toastCtrl.create({
+            message: str,
+            duration: 2000,
+            position: "top"
+        });
+        toast.present();
+    }
 }
